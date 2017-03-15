847ff5e1e6d61fd660537470b0ed8f648ff3d721
Tianpei Wang <tianpei3@gmail.com>
2017-03-14 19:20:32 -0500

    resize and make a video

---
+++ image_resize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 10:02:36 2017
+
+@author: wangtianpei
+"""
+dir_path = "/Users/wangtianpei/Desktop/Chicago_png/"
+import PIL
+from PIL import Image
+for i in range(535,536):
+    image = Image.open(dir_path+'flow_{}.png'.format(i))
+    img = image.resize((2100,1358), PIL.Image.ANTIALIAS)
+    img.save(dir_path+'flow_test_{}.png'.format(i))
---
+++ video.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  5 22:28:27 2017
+
+@author: wangtianpei
+"""
+dir_path = "/Users/wangtianpei/Desktop/Chicago_png/"
+final_files=[]
+for i in range(671):
+    final_files.append(dir_path+'flow_test_{}.png'.format(i))
+import imageio
+images = []
+for file in final_files:
+    images.append(imageio.imread(file))
+imageio.mimsave(dir_path+'Trips_Time series.mp4', images,fps=8)
